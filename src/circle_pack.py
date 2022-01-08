f6d4fc04c263effc8a32675cc97e5770c8927634
Micah Hutchens <mhutchens21144@gmail.com>
2022-01-08 12:46:23 -0700

    Added initial circle generation

---
+++ src/circle_pack.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2 as cv
+import graph as gr
+from matplotlib import pyplot as plt
+
+
+def sort_nodes(graph_obj):
+    # Sort the indices
+    ind = np.argsort(graph_obj.nodeOutDegrees)
+    ind = np.flip(ind)
+    return ind
+
+
+def generate_circles(graph_obj, indices, width=800, height=600):
+    circle_attr = np.random.randint([0, 0, 0], high=[width, height, 1], size=(indices.shape[0], 3))
+    circle_attr[:, 2] = graph_obj.nodeOutDegrees[indices]
+    image = np.zeros((height, width, 3), dtype='uint8')
+    colors = np.random.randint([0, 0, 0], high=[255, 255, 255], size=(indices.shape[0], 3))
+    for idx, row in enumerate(circle_attr):
+        color = (int(colors[idx, 0]), int(colors[idx, 1]), int(colors[idx, 2]))
+        thickness = -1
+        center = (circle_attr[idx, 1], circle_attr[idx, 0])
+        radius = int(circle_attr[idx, 2] * .4)
+        if radius < 3:
+            radius = 3
+        if radius > 40:
+            radius = 40
+        image = cv.circle(image, center, radius, color, thickness)
+    plt.subplot(121), plt.imshow(image, cmap='gray')
+    plt.title('circles'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
+if __name__ == "__main__":
+    fname = '../resources/WitcherNetwork.csv'
+    graph = gr.Graph()
+    graph.read_file(fname, delim=';')
+    indices = sort_nodes(graph)
+    print(graph.nodeOutDegrees[indices])
+    generate_circles(graph, indices)
